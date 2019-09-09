e6018f806148cc1652a5174814cc24fa74f2b1dc
JQ <jesquintero@yahoo.com>
2019-09-09 09:52:51 -0300

    usando .replace() y .append() y . prepend()

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,13 @@
 /*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var awesomeThoughts = " I am Mitchell and I am AWESOME"; //definición de una variane con texto asignado
+
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN"); //acá usamos la función replace("palabra_vieja", "palabra_nueva")
+
+$("#main").append(funThoughts); //.append es una función de JQuery y el $ indica a JS que trabajaremos con JQuery
+
+*/
+
+var formattedName = HTMLheaderName.replace("%data%", "Jesus Mitchell Quintero Ruiz");
+var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+
+$("#header").append(formattedName + formattedRole);
